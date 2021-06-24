2b8c4505232fb6af3c361082230aaeddcf8e7155
DESKTOP-H4NHA26\Mina <mina.nabil@momkn.com.eg>
2021-06-24 14:12:56 +0200

    Revert "Add New"
    This reverts commit e537f2f2ba6d016d717296b89f9ce7f05b84b7ad.

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -26,7 +26,7 @@ namespace IdentityServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the .
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option =>

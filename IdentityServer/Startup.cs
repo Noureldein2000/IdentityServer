e537f2f2ba6d016d717296b89f9ce7f05b84b7ad
DESKTOP-H4NHA26\Mina <mina.nabil@momkn.com.eg>
2021-06-24 14:09:06 +0200

    Add New

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -26,7 +26,7 @@ namespace IdentityServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the .
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option =>

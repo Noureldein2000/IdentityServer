cec0afba1cc8dd14144c672d2142af1330ad85fc
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2021-06-24 15:27:52 +0200

    Add Channel Entites with its relation

---
+++ IdentityServer/Entities/ChannelType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Entities
+{
+    public class ChannelType : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string ArName { get; set; }
+        public int Version { get; set; }
+        public int ChannelCategoryID { get; set; }
+
+        //Relational Entity
+        public ICollection<Channel> Channels { get; set; }
+        public ChannelCategory ChannelCategory { get; set; }
+        public ICollection<AccountChannelType> AccountChannelTypes { get; set; }
+
+    }
+}

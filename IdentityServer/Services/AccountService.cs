f6e289ff97290d4b5bb85e8b1baa052893441e1a
ALAHLYMOMKN\nour.hussein <nour.hussein@momkn.com.eg>
2021-07-04 06:27:41 +0200

    build action api  for Change Status account request

--- IdentityServer/Services/AccountService.cs
+++ IdentityServer/Services/AccountService.cs
@@ -2,6 +2,7 @@
 using IdentityServer.Entities;
 using IdentityServer.Infrastructure;
 using IdentityServer.Repositories.Base;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,13 +13,23 @@ namespace IdentityServer.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly IBaseRepository<AccountRequest, int> _accountRequests;
+        private readonly IBaseRepository<AccountOwner, int> _accountOwner;
+        private readonly IBaseRepository<Account, int> _account;
+        private readonly IBaseRepository<Activity, int> _activity;
         private readonly IUnitOfWork _unitOfWork;
         public AccountService(IBaseRepository<AccountRequest, int> accountRequests,
+            IBaseRepository<AccountOwner, int> accountOwner,
+            IBaseRepository<Account, int> account,
+                IBaseRepository<Activity, int> activity,
             IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
             _accountRequests = accountRequests;
+            _accountOwner = accountOwner;
+            _account = account;
+            _activity = activity;
+            _unitOfWork = unitOfWork;
         }
 
         public AccountRequestDTO Add(AccountRequestDTO accountRequestDto)
@@ -40,9 +51,40 @@ namespace IdentityServer.Services
             return MapEntityToDto(entityRequest);
         }
 
-        public AccountRequestStatus ChangeAccountRequestStatus(AccountRequestStatus status, int id)
+        public AccountRequestStatus ChangeAccountRequestStatus(int id, AccountRequestStatus status)
         {
-            throw new NotImplementedException();
+            var currentAccountRequest = _accountRequests.GetById(id);
+            currentAccountRequest.AccountRequestStatus = status;
+
+            if (status == AccountRequestStatus.Approved)
+            {
+                Account account = new Account();
+                account.Address = currentAccountRequest.Address;
+                account.Active = 0;
+                account.TaxNo = currentAccountRequest.TaxNo;
+                account.CommercialRegistrationNo = currentAccountRequest.CommercialRegistrationNo;
+                account.Name = currentAccountRequest.AccountName;
+
+                account.Activity = _activity.GetById(currentAccountRequest.ActivityID).NameAr;
+                //account.Activity = currentAccountRequest.Activity.NameAr; //error object null refrence
+
+                //account.CreatedBy = _userManager // we need find current user Id to add it
+                account = _account.Add(account);
+
+                _unitOfWork.SaveChanges();
+
+                AccountOwner accountOwner = new AccountOwner();
+                accountOwner.Name = currentAccountRequest.OwnerName;
+                accountOwner.Address = currentAccountRequest.Address;
+                accountOwner.Email = currentAccountRequest.Email;
+                accountOwner.Mobile = currentAccountRequest.Mobile;
+                accountOwner.NationalID = currentAccountRequest.NationalID;
+                accountOwner.AccountID = account.ID;
+                _accountOwner.Add(accountOwner);
+            }
+
+            _unitOfWork.SaveChanges();
+            return status;
         }
 
         public IEnumerable<AccountRequestDTO> GetAccountRequests(AccountRequestStatus status = AccountRequestStatus.UnderProcessing)
